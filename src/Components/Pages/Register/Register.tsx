d1222c6ca1c89bc8df0f0b4bff83229927fc44e9
iamashwini78 <ashwini@blisstech.in>
2025-04-11 17:14:05 +0530

    bg color change

--- src/Components/Pages/Register/Register.tsx
+++ src/Components/Pages/Register/Register.tsx
@@ -4,7 +4,7 @@ export default function Register() {
   return (
     <>
      
-     <div className="grid grid-cols-2 ">
+     <div className="grid grid-cols-1 lg:grid-cols-2 ">
       <div>
       <p className="m-5 flex logo-text">
         <img src="/assets/arrow.png" className="w-5 h-5 mr-3" />
